2d4eacc7cf068f73aef66606c1957fedbb33de4c
Caroline <palomamabel@orange.fr>
2022-03-17 14:37:47 +0100

    Amélioration du code par l'ajout de fonctions afin de rendre le script plus lisible.

--- SiteWeb.py
+++ SiteWeb.py
@@ -6,18 +6,16 @@ from math import *
 import pandas as pd
 
 # initialisation de variables
-titres = [] # Tableau dans lequel nous rangeons les titres
-urlProduits = [] # tableau dans lequel nous rangeons les urls relatives
-listeUrlProduitParCategorie = [] # tableau dans lequel nous rangeons les urls absolues après reconstitution
-#nbrTotalPage = []
-nbrTotalProduitsCat =[]
-listeCategories = [] # Urls relatives de toutes les categories du site
-listeUrlCategorie = [] # #Urls absolues de toutes les categories du site
-listeUrlCategorieDyn = []
-varDynamique = "page-" # initialisation d'une variable  utilisée pour transformer certaines urls en urls dynamiques
-# Declaration des tableaux vides pour le rangement des futures données et la mise en page CSV
+titres = []  # Tableau dans lequel nous rangeons les titres
+liste_categories = []  # Urls relatives de toutes les categories du site
+liste_url_categorie = []  # Urls absolues de toutes les categories du site
+url_produits = []  # rangement des urls relatives des produits
+liste_url_produit_par_categorie = []  # rangement des urls absolues des produits
+var_dynamique = "page-"  # initialisation d'une variable pour créer des urls dynamiques
 
-product_page_url = []
+# Declaration des tableaux vides pour le rangement des données et la mise en page CSV
+
+product_page_Url = []
 universal_product_code = []
 title = []
 price_including_tax = []
@@ -29,151 +27,185 @@ review_rating = []
 image_url = []
 
 # Initialisation des variables pour accéder au site et extraire les données
-url = 'http://books.toscrape.com'
-r = requests.get(url)
+URL = 'https://books.toscrape.com'
+r = requests.get(URL)
 soup = BeautifulSoup(r.content.decode('utf-8', 'ignore'), 'html.parser')
 
-
-
-# création d'une boucle qui recherche et recupère l'ensemble des urls des catégories du site Book To Scrape
-for linkCategorie in soup.find("ul", class_='nav nav-list').ul.findAll("a"):
-   listeCategories.append(linkCategorie.get('href'))  # j'ajoute ces urls relatives à la liste listeCategories initialisée plus haut
+# création d'une boucle qui recupère l'ensemble des urls des catégories du site
+for link_category in soup.find("ul", class_='nav nav-list').ul.findAll("a"):
+    liste_categories.append(
+        link_category.get('href'))  # ajout des urls relatives à la liste liste_categories initialisée plus haut
 
 # Transformation des urls relatives des categories en urls absolues pour pouvoir y accéder
-for i in listeCategories:  # Création d'une boucle qui parcourt les urls relatives ligne à ligne du tableau listeCategories
-    totalCategorie = (urllib.parse.urljoin(url, i)) # on assemble les 2 morceaux d'url à l'aide d'urljoin
-    listeUrlCategorie.append(totalCategorie) # récupération des urls absolues dans le tableau listeUrlProduitParCategorie
-print("Merci de patienter pendant l'extraction des catégories du site https://books.toscrape.com/...")
-for urlCategorieAbsolue in listeUrlCategorie: # on parcourt les urls absolues du tableau contenant la liste des urls absolues
-    url = urlCategorieAbsolue # on indique que la variable url doit prendre comme valeur l'url absolue des categories contenues dans le tableau
+for i in liste_categories:  # Création d'une boucle qui parcourt les urls relatives ligne à ligne du tableau liste_categories
+    total_categorie = (urllib.parse.urljoin(URL, i))  # on assemble les 2 morceaux d'url à l'aide d'urljoin
+    liste_url_categorie.append(total_categorie)  # récupération des urls absolues dans le tableau liste_url_categorie
+print("Merci de patienter pendant l'extraction des catégories du site", URL, "...")
+
+for url_categorie_absolue in liste_url_categorie:  # on parcourt les urls absolues du tableau contenant la liste des urls absolues
+    url = url_categorie_absolue  # on indique que la variable url doit prendre comme valeur l'url absolue des categories contenues dans le tableau
     response = requests.get(url)
     html = response.content.decode('utf-8', 'ignore')
     soup = BeautifulSoup(html, 'html.parser')
     page = 1
     # Calcul du nombre total de produits et du nombre total de pages pour chaque categorie
-    nbrMaxProdPage = 20  # nombre maximum de produits affichés par page
-    nbrProduit = soup.form.strong.text  # extraction du nombre de produits total  dans le code source
-    nbrProduits = int(nbrProduit)  # transformation du contenu de la balise string en int()
-    totalProduitsPage = nbrProduits / nbrMaxProdPage  # division du nombre total de produits de la catégorie par le nombre maximum de produits par page
-    nbrPage = ceil(totalProduitsPage) # utilisation de la fonction ceil() pour un arrondi supérieur afin de ne pas oublier de pages
+    nbr_max_prod_page = 20  # nombre maximum de produits affichés par page
+    nbr_produit = soup.form.strong.text  # extraction du nombre de produits total  dans le code source
+    nbr_produits = int(nbr_produit)  # transformation du contenu de la balise string en int()
+    total_produits_page = nbr_produits / nbr_max_prod_page  # division du nombre total de produits de la catégorie par le nombre maximum de produits par page
+    nbr_page = ceil(
+        total_produits_page)  # utilisation de la fonction ceil() pour un arrondi supérieur afin de ne pas oublier de pages
 
-    if nbrProduits < 20: # instruction conditionnelle indiquant que si une page catégorie possède moins de 20 produits alors...
-        url = urlCategorieAbsolue  # la variable url contient l'URL absolue et non dynamique de la catégorie
+    if nbr_produits < 20:  # instruction conditionnelle indiquant que si une page catégorie possède moins de 20 produits alors...
+        url = url_categorie_absolue  # la variable url contient l'URL absolue et non dynamique de la catégorie
         response = requests.get(url)
         html = response.content.decode('utf-8', 'ignore')
         soup = BeautifulSoup(html, 'html.parser')
-        #print(url)
 
-        for link in soup.select("h3 > a"):  # création d'une boucle qui recherche et recupère l'ensemble urls des produits à l'intérieur des balises h3
-            urlProduits.append(link.get('href'))  # j'aoute ces urls relatives à la liste urlProduits initialisée plus haut
+        for link in soup.select(
+                "h3 > a"):  # création d'une boucle qui recherche et recupère l'ensemble urls des produits à l'intérieur des balises h3
+            url_produits.append(
+                link.get('href'))  # j'aoute ces urls relatives à la liste url_produits initialisée plus haut
 
-    while page != nbrPage + 1: # création d'une boucle qui va itérer jusqu'à atteindre le nombre de pages total pour une categorie donnée.
-        if nbrPage > 1: # création instruction conditionnelle pour ne concerner que les catégories possédant plusieurs pages
-            urlD = (urllib.parse.urljoin(urlCategorieAbsolue,varDynamique))  # création url dynamique assemble les 2 morceaux d'url à l'aide d'urljoin
-            urlDynamique = urlD + f"{page}.html" # utilisation des f-strings qui permettent à {page} de recevoir la valeur actualisée de la page,
-            url = urlDynamique  # la variable url contiendra l'URL dynamique de la page
+    while page != nbr_page + 1:  # création d'une boucle qui va itérer jusqu'à atteindre le nombre de pages total pour une categorie donnée.
+        if nbr_page > 1:  # création instruction conditionnelle pour ne concerner que les catégories possédant plusieurs pages
+            url_dyn = (urllib.parse.urljoin(url_categorie_absolue,
+                                            var_dynamique))  # création url dynamique assemble les 2 morceaux d'url à l'aide d'urljoin
+            url_dynamique = url_dyn + f"{page}.html"  # utilisation des f-strings qui permettent à {page} de recevoir la valeur actualisée de la page,
+            url = url_dynamique  # la variable url contiendra l'URL dynamique de la page
             response = requests.get(url)
             html = response.content.decode('utf-8', 'ignore')
-            soup = BeautifulSoup( html, 'html.parser')
+            soup = BeautifulSoup(html, 'html.parser')
 
-            for link in soup.select("h3 > a"):  # création d'une boucle qui recherche et recupère l'ensemble urls des produits à l'intérieur des balises h3
-                urlProduits.append(link.get('href'))  # j'aoute ces urls relatives à la liste urlProduits initialisée plus haut
-
+            for link in soup.select(
+                    "h3 > a"):  # création d'une boucle qui recherche et recupère l'ensemble urls des produits à l'intérieur des balises h3
+                url_produits.append(
+                    link.get('href'))  # j'aoute ces urls relatives à la liste url_produits initialisée plus haut
 
         page = page + 1  # augmentation de la valeur de la page de 1 à la fin de chaque itération pour passer à la page suivante.
-print("Opération effectuée.")
-print("Veuillez patienter quelques instants, nous récupérons les données pour chaque produit et les écrivons dans un fichier csv:")
-def fonction_extraction_donnees_livre(baseUrl, soup, url):
-    # produit description
-    description = soup.find("article", class_="product_page").findAll(["p"])
 
+print("Opération effectuée.")
+print("Veuillez patienter pendant la récupération des données produit pour chaque catégorie dans un fichier csv:")
+
+
+def element_extraction(soup):
     # récupération des éléments du tableau "table" de la page produit dans une liste
     element = soup.find('table', class_="table table-striped").findAll(["td"])
+    return element
 
-    # category
-    cat = soup.find('ul', class_="breadcrumb").findAll(["li"])
 
-    # product_page_url
-    url = url
-    #print(url)
-
-    # UPC
+def UPC_extraction(element):
     UPC = element[0]
-    #print(UPC.text)
+    return UPC
 
-    # title
-    titre = soup.find('h1')
-    #print(titre.text)
 
+def price_incl_extraction(element):
     # price_including_tax
     price_incl = element[3]
-    price_including = price_incl.encode('utf-8', errors='ignore')
-    #print(price_incl.text)
+    return price_incl
 
+
+def price_excl_extraction(element):
     # price_excluding_tax
     price_excl = element[2]
-    price_excluding = price_excl.encode('utf-8', errors='ignore')
-    #print(price_excl.text)
+    return price_excl
 
+def stock_extraction(element):
     # number available
     stock = element[5]
-    #print(stock.text)
+    return stock
 
-    # product_description
-    description = description[3]
-    #print(description.text)
 
-    # category
-    catego = cat[2]
-    #print(catego.text)
-
+def review_extraction(element):
     # Review_rating
     review = element[6]
-    #print(review.text)
+    return review
 
+
+def url_extraction(url):
+    product_page_url = url
+    return product_page_url
+
+
+def title_extraction(soup):
+    # title
+    titre = soup.find('h1')
+    return titre
+
+
+def description_extraction(soup):
+    description_soup = soup.find("article", class_="product_page").findAll(["p"])
+    description = description_soup[3]
+    return description
+
+
+def cat_extraction(soup):
+    cat = soup.find('ul', class_="breadcrumb").findAll(["li"])
+    catego = cat[2]
+    return catego
+
+
+def image_extraction(base_url, soup):
     # image_url
     image = soup.find('img')
-    #print(baseUrl + image["src"])
+    return image
 
-    return UPC, titre, price_incl, price_excl, stock, description, catego, review, image
-def save_to_csv(product_page_url, universal_product_code, title,price_including_tax, price_excluding_tax, number_available, product_description, category, review_rating, image_url, tempCategory):
-    BookToScrape = pd.DataFrame(
-        {'Product_page_url': product_page_url, 'Universal_product_code': universal_product_code, 'Title': title,
+
+def extraction_donnees_site_web(base_url, soup, url, element):
+    url_extraction(url)
+    UPC_extraction(element)
+    title_extraction(soup)
+    price_incl_extraction(element)
+    price_excl_extraction(element)
+    stock_extraction(element)
+    description_extraction(soup)
+    cat_extraction(soup)
+    review_extraction(element)
+    image_extraction(base_url, soup)
+
+    return url_extraction(url), UPC_extraction(element), title_extraction(soup), price_incl_extraction(element), price_excl_extraction(element), stock_extraction(element), description_extraction(soup), cat_extraction(soup), review_extraction(element), image_extraction(base_url, soup)
+
+def save_to_csv(product_page_Url, universal_product_code, title, price_including_tax, price_excluding_tax,
+                number_available, product_description, category, review_rating, image_url, temp_category):
+    book_to_scrape = pd.DataFrame(
+        {'Product_page_url': product_page_Url, 'Universal_product_code': universal_product_code, 'Title': title,
          'Price_including_tax': price_including_tax, 'Price_excluding_tax': price_excluding_tax,
          'Number_available': number_available, 'Product_description': product_description, 'category': category,
          'Review_rating': review_rating, 'Image_url': image_url})
-    filename = '{0}.csv'.format(tempCategory)
+    filename = '{0}.csv'.format(temp_category)
     print(filename)
-    BookToScrape.to_csv(filename, index=False, encoding='utf-8')
+    book_to_scrape.to_csv(filename, index=False, encoding='utf-8')
 
-#assemblage des urls relatives pour créer des urls absolues des produits
-url1 = "https://books.toscrape.com/catalogue/category/books/romance_8/index.html"
-for i in urlProduits:  # on parcourt les urls relatives ligne à ligne de tous les produits
-    totalUrl = (urllib.parse.urljoin(url1, i)) # on assemble les 2 morceaux d'url à l'aide d'urljoin
-    #print(totalUrl)
-    listeUrlProduitParCategorie.append(totalUrl) # récupération des urls absolues dans le tableau listeUrlProduitParCategorie
-    #print(listeUrlProduitParCategorie)
-tempCategory = None
-for urli in listeUrlProduitParCategorie: # on parcourt les urls absolues des produits dans le tableau
-    url = urli # on indique que la variable url doit prendre comme valeur les urls absolues des produits
+
+# assemblage des urls relatives pour créer des urls absolues des produits
+
+for i in url_produits:  # on parcourt les urls relatives ligne à ligne de tous les produits
+    total_url = (urllib.parse.urljoin(url_categorie_absolue, i))  # on assemble les 2 morceaux d'url à l'aide d'urljoin
+    liste_url_produit_par_categorie.append(
+        total_url)  # récupération des urls absolues dans le tableau liste_url_produit_par_categorie
+
+temp_category = None
+for urli in liste_url_produit_par_categorie:  # on parcourt les urls absolues des produits dans le tableau
+    url = urli  # on indique que la variable url doit prendre comme valeur les urls absolues des produits
     response = requests.get(url)
     html = response.content.decode('utf-8', 'ignore')
     soup = BeautifulSoup(html, 'html.parser')
-    baseUrl = "https://books.toscrape.com/" # variable pour reconstitution url absolue des images
+    base_url = "https://books.toscrape.com/"  # variable pour reconstitution url absolue des images
 
-    UPC, titre, price_including, price_excluding, stock, description, catego, review, image = fonction_extraction_donnees_livre(baseUrl, soup, url)
+    element = element_extraction(soup)
+    product_page_url, UPC, titre, price_incl, price_excl, stock, description, catego, review, image = extraction_donnees_site_web(base_url, soup, url, element)
     format_catego = str(catego.text).strip()
 
-    if not tempCategory: # si tempCategory n'est pas defini ou est vide
-        tempCategory = format_catego #Alors on affecte la valeur de la variable catego à la variable tempCategory
+    if not temp_category:  # si temp_category n'est pas defini ou est vide
+        temp_category = format_catego  # Alors on affecte la valeur de la variable catego à la variable temp_category
 
     # ajout des données aux tableaux créées plus haut
-    if tempCategory != format_catego:
-        save_to_csv(product_page_url, universal_product_code, title, price_including_tax, price_excluding_tax,
-                    number_available, product_description, category, review_rating, image_url, tempCategory)
+    if temp_category != format_catego:
+        save_to_csv(product_page_Url, universal_product_code, title, price_including_tax, price_excluding_tax,
+                    number_available, product_description, category, review_rating, image_url, temp_category)
 
-        tempCategory = format_catego
-        product_page_url = []
+        temp_category = format_catego
+        product_page_Url = []
         universal_product_code = []
         title = []
         price_including_tax = []
@@ -183,19 +215,19 @@ for urli in listeUrlProduitParCategorie: # on parcourt les urls absolues des pro
         category = []
         review_rating = []
         image_url = []
-    product_page_url.append(url)
+
+    product_page_Url.append(url)
     universal_product_code.append(UPC.text)
     title.append(titre.text)
-    price_including_tax.append(price_including.text)
-    price_excluding_tax.append(price_excluding.text)
+    price_including_tax.append(price_incl.text)
+    price_excluding_tax.append(price_excl.text)
     number_available.append(stock.text)
     product_description.append(description.text)
     category.append(format_catego)
     review_rating.append(review.text)
-    image_url.append(baseUrl + image["src"].replace("../", ""))
+    image_url.append(base_url + image["src"].replace("../", ""))
 
-save_to_csv(product_page_url, universal_product_code, title,price_including_tax, price_excluding_tax, number_available, product_description, category, review_rating, image_url, tempCategory)
+save_to_csv(product_page_Url, universal_product_code, title, price_including_tax, price_excluding_tax, number_available,
+            product_description, category, review_rating, image_url, temp_category)
 
-print("L'opération s'est bien déroulée. Merci pour votre patience ;-)")
-
-
+print("Opération terminée. Tout s'est bien déroulé. Merci pour votre patience ;-)")
